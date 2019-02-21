a463cd5839aa1cc83b99fa3c507ba119ddfb5e07
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-22 00:26:33 +0100

    Update HTML struct

--- main.js
+++ main.js
@@ -9,11 +9,14 @@ const ul = document.querySelector('ul');
 const create = (e) => {
     e.preventDefault();
     const text = input.value;
-    if (!input.value === "") {
-        const li = document.createElement("li");
-        const check = document.createElement("input");
+    const li = document.createElement("li");
+    const check = document.createElement("input");
+    const span = document.createElement("span");
+
+    if (input.value !== "") {
+
         check.type = "checkbox";
-        const span = document.createElement("span");
+
         ul.appendChild(li);
         li.appendChild(check);
         li.appendChild(span);
@@ -27,11 +30,15 @@ const create = (e) => {
     check.addEventListener('click', () => {
         check.classList.toggle("checked");
         check.classList.contains("checked") ? span.style.textDecoration = "line-through" : span.style.textDecoration = "none";
+
     })
+    done()
 }
 
+const done = () => {
 
 
+}
 
 
 
