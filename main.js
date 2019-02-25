b29cedaded225c79dbfc7b2b08c900acdeb8d347
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-25 22:55:10 +0100

    Update style

--- main.js
+++ main.js
@@ -1,79 +1,59 @@
+const toDoList = [];
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const ul = document.querySelector('ul');
-const doneUl = document.querySelector('doneUl');
-let doneListCheck;
+const listItems = document.getElementsByClassName('taskList');
+const numberTask = document.querySelector('h2 span');
+const searchInput = document.getElementById("searchInput")
 
 
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasks = toDoList;
+    tasks = tasks.filter(li =>
+        li.textContent.toLowerCase().includes(searchText));
+    ul.textContent = "";
+    tasks.forEach(li => ul.appendChild(li))
+    if (searchInput !== "") {
+        tasks.forEach(li => li.style.backgroundColor = "red")
 
-const create = (e) => {
-    e.preventDefault();
-    const text = input.value;
-    const li = document.createElement("li");
-    const check = document.createElement("input");
-    const span = document.createElement("span");
-
-    if (input.value !== "") {
-
-        check.type = "checkbox";
-
-        ul.appendChild(li);
-        li.appendChild(check);
-        li.appendChild(span);
-        doneListCheck = span.textContent = text;
-        li.insertBefore(check, span)
-        input.value = "";
     } else {
-        alert("Nie można dodać pustego zadania!");
+        tasks.forEach(li => li.style.backgroundColor = "transparent");
     }
+}
 
-    check.addEventListener('click', () => {
-        check.classList.toggle("checked");
-        span.classList.toggle("checked");
-        check.checked = true;
-        if (check.classList.contains("checked")) {
-            span.style.textDecoration = "line-through";
-            done();
-            ul.removeChild(li);
+const removeTask = (e) => {
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1); //clean delete piece array
+    numberTask.textContent = toDoList.length;
+    renderList();
+}
 
-        } else {
-            span.style.textDecoration = "none";
-            check.checked = false;
-        }
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if (titleTask === "") return; //block add empty task
+
+    const task = document.createElement('li');
+    task.className = "taskList";
+    task.innerHTML = titleTask + "<button> Usuń</button>";
+    toDoList.push(task); //send task to array
+    renderList();
+    ul.appendChild(task);
+    input.value = "";
+    numberTask.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask)
+}
+
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement);
     })
 
 }
-
-const done = () => {
-    const doneUl = document.getElementById("doneUl");
-    const li = document.createElement("li");
-    const check = document.createElement("input");
-    const span = document.createElement("span");
-    const spanElement = document.querySelector("span");
-    check.type = "checkbox";
-    check.checked = true;
-// dopisz funkcje z warunkiem wywołującą 2 funkcje do zrobienia i zrobione
-
-    if (check.checked = true) {
-        // check.checked = true;
-        if (check.checked = true) {
-            doneUl.appendChild(li);
-            li.appendChild(check);
-            li.appendChild(span);
-            span.textContent = doneListCheck;
-            li.insertBefore(check, span)
-            input.value = "";
-            span.style.textDecoration = "line-through";
-        }
-
-    } else {
-        check.checked = false;
-        doneUl.removeChild(li);
-    }
-
-}
-
-
-
-form.addEventListener('submit', create)
+form.addEventListener('submit', addTask)
+searchInput.addEventListener('input', searchTask)
